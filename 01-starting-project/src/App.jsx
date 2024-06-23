7283c716c4282df43b86f406824cb8b888de49f5
jiaakmal <160829841+jiaakmal@users.noreply.github.com>
2024-06-23 17:01:45 +0500

    updating app.jsx file and creating new data.js file

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,12 +1,33 @@
+import reactImg from './assets/react-core-concepts.png';
+import { CORE_CONCEPTS } from './data.js';
 const reactDescription=['Fundamental','Crutial','Core'];
 function genRandomInt(max){
   return Math.floor(Math.random()*(max+1));
+}
+// function CoreConcept(props){
+//   return(
+//     // <li>
+//     //   <h3>{props.title}</h3>
+//     //   <p>{props.description}</p>
+//     //   <img src={props.image} alt={props.title} />
+//     // </li>
+    
+//   );
+// }
+function CoreConcept({image,title,description}){
+  return(
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
 }
 
 function Header(){
   const description=reactDescription[genRandomInt(2)];
   return(<header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
           {description} React concepts you will need for almost any app you are
@@ -15,15 +36,34 @@ function Header(){
       </header>);
 
 }
+
+
 function App() {
   return (
     <div>
       <Header/>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+        <h2>Core Concepts</h2>
+        <ul>
+          <CoreConcept 
+          title={CORE_CONCEPTS[0].title}
+          description={CORE_CONCEPTS[0].description} 
+          image={CORE_CONCEPTS[0].image}
+          />
+          <CoreConcept 
+          title={CORE_CONCEPTS[1].title}
+          description={CORE_CONCEPTS[1].description} 
+          image={CORE_CONCEPTS[1].image}
+          />
+          <CoreConcept{...CORE_CONCEPTS[2]}/>
+          <CoreConcept{...CORE_CONCEPTS[3]}/>
+      
+         
+        </ul>
+        </section>
       </main>
     </div>
-  );
+    );
 }
-
 export default App;
---
+++ 01-starting-project/src/data.js
@@ -0,0 +1,28 @@
+import componentImg from './assets/components.png'
+import jsxUI from './assets/jsx-ui.png'
+import config from './assets/config.png'
+import state from './assets/state-mgmt.png'
+
+export const CORE_CONCEPTS =[
+    {
+        title:'Component',
+        description:'The core UI building blocks.',
+        image:componentImg
+    },
+    {
+        title:'JSX',
+        description:'The core UI building blocks.',
+        image:jsxUI
+    },
+    {
+        title:'Config',
+        description:'The core UI building blocks.',
+        image:config
+    },
+    {
+        title:'State',
+        description:'The core UI building blocks.',
+        image:state
+    }
+]
+
